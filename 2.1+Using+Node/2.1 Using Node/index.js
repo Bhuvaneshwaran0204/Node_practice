e4ea4eaba1654c236643000694c48ef7b20d55a9
Bhuvaneshwaran <bhuvaneshwaranaj@gmail.com>
2025-05-23 09:02:44 +0530

    Initial commit

---
+++ 2.1+Using+Node/2.1 Using Node/index.js
@@ -0,0 +1,1 @@
+console.log("Welcome back Mr.Bhuvaneshwaran");
---
+++ 2.3+NPM/index.js
@@ -0,0 +1,13 @@
+// var generateName = require('sillyname');
+
+import generateName from 'sillyname';
+  
+var sillyName = generateName();
+console.log(`this is my name ${sillyName}.`);
+
+// import superheroes from 'superheroes';
+
+// const superheroName = superheroes.random(); // Get a random superhero name
+
+// console.log(superheroName);
+// console.log(`Hello, ${superheroName}!`);
